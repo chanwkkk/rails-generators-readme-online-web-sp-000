942d2c802beb7ef6627e16263f3ae43a4de66d7c
Yingqi Chen <chanwk18@hotmail.com>
2019-09-16 07:29:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190916072527) do
+ActiveRecord::Schema.define(version: 20190916072841) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "bio"
   end
 
   create_table "categories", force: :cascade do |t|
